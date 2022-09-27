61ed0a718e589b42d5599df110b7d46594c0df53
Abdulmenam <93938541+Abdulmenam@users.noreply.github.com>
2022-09-27 11:18:26 +0300

    submit final task of module 3

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+    	String numberAsString = String.valueOf(number);
+    	int sum = 0;
+    	   for (int i = 0; i < numberAsString.length(); i++) {
+	    	    sum = number % 10 + sum;
+	            number /=10;
+    	   }
+    System.out.println(sum);
     }
 }
